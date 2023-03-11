231a79910a853113885f3421d12a0f2aedc04364
Annebula77 <goharikv@gmail.com>
2023-03-12 01:49:18 +0300

    added burgerConstructor

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,13 @@
 import AppHeader from "../app-header/app-header";
 import Main from "../main/main";
+import styles from './app.module.css'
 
 function App() {
   return (
-    <>
+    <section className={styles.app}>
       <AppHeader />
       <Main />
-    </>
+    </section>
   );
 }
 
